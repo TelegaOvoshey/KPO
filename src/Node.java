797998ba300c46e9592b5a47b1908736a195040f
TelegaOvoshey <Redeye.drd@gmail.com>
2017-10-30 21:36:19 +0400

    Some basic methods

--- src/Node.java
+++ src/Node.java
@@ -1,14 +1,32 @@
+import java.util.Collection;
+
+
 public class Node {
-    Node[] children;
-    int nodeNumber;
+    private int id;//id ноды
+    private int pid;//id родителя
+    private Collection<Node> children; //Потомки
+    private Node parent;//Родитель
 
-    public Node () {
-
+    public Node(int id, int pid) {
+        this.id = id;
+        this.pid = pid;
     }
 
-    public Node setNumberOfChildren(int number) {
-        this.children = new Node[number];
-        return this;
+    public void accept(Node[] nodes) {
+        for(int i = 0; i < nodes.length; i++ ) {
+            if(nodes[i].pid == id) {
+                addChild(nodes[i]);
+                nodes[i].accept(nodes);
+            }
+        }
     }
 
+    public void addChild(Node child){
+        child.parent = this;
+        getChildren().add(child);
+    }
+
+    public Collection<Node> getChildren() {
+        return children;
+    }
 }
--- src/TreeBuilder.java
+++ src/TreeBuilder.java
@@ -1,2 +1,11 @@
+import java.util.Random;
+
 public class TreeBuilder {
+    public static void CreateTree() {
+        Random randomGenerator = new Random();
+        int randomChildQuantity, m = 6, N = 200;
+        while (N > 200) {
+            randomChildQuantity = randomGenerator.nextInt(m);
+        }
+    }
 }
